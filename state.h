f6ee2200184a617984776a2907844fae1c5b6ef5
pauls <paul.souteyrat@timeplanner-app.com>
2022-01-21 11:46:49 +0100

    Adding override keyword

--- state.h
+++ state.h
@@ -26,70 +26,70 @@ class State0 : public State {
 public:
     State0() : State("I0") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 class State1 : public State {
 public:
     State1() : State("I1") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 class State2 : public State {
 public:
     State2() : State("I2") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 class State3 : public State {
 public:
     State3() : State("I3") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 class State4 : public State {
 public:
     State4() : State("I4") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 class State5 : public State {
 public:
     State5() : State("I5") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 class State6 : public State {
 public:
     State6() : State("I6") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 class State7 : public State {
 public:
     State7() : State("I7") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 class State8 : public State {
 public:
     State8() : State("I8") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 class State9 : public State {
 public:
     State9() : State("I9") {};
 
-    bool Transition(Automaton &automate, Symbol *symbol);
+    bool Transition(Automaton &automate, Symbol *symbol) override;
 };
 
 #endif //TP_4IFA_GL_STATE_H
